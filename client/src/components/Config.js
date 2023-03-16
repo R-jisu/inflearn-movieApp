91ddd98a608138f3b613869eae4f9600e5635ae8
Ryu jisu <wltn1418@gmail.com>
2023-03-16 09:02:24 +0900

    MainImage

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "1fcd5ebc68de73e3c682786dadc576c4";
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
